8341f969e8d35a23604ed0d5b0e7d47c5d79a2ec
Florent Clairambault <florent@clairambault.fr>
2013-07-16 03:00:37 +0200

    I finally managed to integrate my other libraries in the project.

--- org.m2mp.web/app/controllers/Application.java
+++ org.m2mp.web/app/controllers/Application.java
@@ -2,6 +2,8 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import org.m2mp.db.*;
+
 
 import views.html.*;
 
@@ -10,5 +12,5 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-  
+	
 }
--- org.m2mp.web/project/Build.scala
+++ org.m2mp.web/project/Build.scala
@@ -5,17 +5,21 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "M2MPWeb"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "1.0"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    "org.m2mp" % "org.m2mp.db" % "1.0-SNAPSHOT"
   )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+  //resolvers += "local2" at "file:///home/florent/.m2/repository"
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(defaultScalaSettings:_*).settings(
+    // Add your own project settings here
+    resolvers += "local2" at "file:///home/florent/.m2/repository"
   )
 
 }

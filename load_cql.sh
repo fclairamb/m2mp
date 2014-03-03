43d98653a7fd7dcfab3897f7d1e822f257c29bf8
Florent Clairambault <florent@clairambault.fr>
2014-03-03 23:37:43 +0100

    CQL loading fix.

--- load_cql.sh
+++ load_cql.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for f in `find cql -type f`; do cat $f | cassandra/bin/cqlsh; done
+for f in `find cql -type f | sort`; do printf "Loading ${f}..." ; cat $f | cassandra/bin/cqlsh; echo " OK" ; done 

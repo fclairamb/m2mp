1d87a2154262955175028bcb17989b1f8d570c26
Florent Clairambault <florent@clairambault.fr>
2014-03-03 23:33:42 +0100

    Adding other unit tests.

--- drone_io.sh
+++ drone_io.sh
@@ -2,6 +2,18 @@
 ./drone_io_start_cassandra.sh
 ./load_cql.sh
 
-cd go/m2mpdb
+DIR `pwd`
+
+# Go
+cd $DIR/go/m2mpdb
 go get
 go test -v -timeout 10s
+
+# Python
+cd $DIR/python
+python tests.py
+
+# Java
+cd $DIR/java/org.m2mp.db
+mvn install
+

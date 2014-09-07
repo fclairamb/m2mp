d0e3ae1e75d5b948a6ea60400820d90fe0c72ee7
Florent Clairambault <florent@clairambault.fr>
2014-09-07 02:47:03 +0200

    Adding back simple log files.

--- go/client-alip/log.go
+++ go/client-alip/log.go
@@ -1,13 +1,27 @@
 package main
 
 import (
-	m2log "github.com/fclairamb/m2mp/go/m2mp-log"
 	logging "github.com/op/go-logging"
+	glog "log"
+	"os"
 )
+
+const LOG_FLAGS = glog.Ldate | glog.Ltime | glog.Lmicroseconds | glog.Lshortfile
 
 var log *logging.Logger
 
+func logFile() *os.File {
+	if file, err := os.OpenFile("client-alip.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0660); err == nil {
+		return file
+	} else {
+		return nil
+	}
+}
+
 func init() {
-	m2log.NoColor = true
-	log = m2log.GetLogger()
+	stdoutBackend := logging.NewLogBackend(os.Stdout, "", glog.LstdFlags|glog.Lshortfile)
+	fileBackend := logging.NewLogBackend(logFile(), "", glog.LstdFlags|glog.Lshortfile)
+	logging.SetBackend(stdoutBackend, fileBackend)
+	glog.SetFlags(LOG_FLAGS)
+	log, _ = logging.GetLogger("m2log")
 }
